07a1afa7d84a2be6ee9ac90778aa9d11a164ecaa
Gaetan Desrues <gaetan.desrues@inria.fr>
2022-10-11 00:51:31 +0200

    new release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="htmlit",
-    version="0.1.008",
+    version="0.1.009",
     author="Gaetan Desrues",
     author_email="gdesrues@gmail.com",
     url="https://github.com/GaetanDesrues/htmlgen",
